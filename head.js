0afdc150cd5ae76332ad2164df1246f4d8a11e99
Z Xian <alex.xian@hotmail.com>
2020-09-15 03:56:24 +0000

    Yay

---
+++ head.js
@@ -0,0 +1,19 @@
+/*
+for the test
+const assertEqual = function(actual, expected) {
+  if (actual === expected){
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+*/
+
+function head(asd) {
+  return asd[0];
+}
+/*
+for test
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+*/
