e23e20d1f834c19f2f8c6bbe1bd1271d9e7de4b5
Z Xian <alex.xian@hotmail.com>
2020-09-15 03:48:35 +0000

    part2

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,12 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected){
-    console.log("🛑🛑🛑 Assertion Passed: " + actual + " === " + expected)
+    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`)
   } else {
-    console.log("✅✅✅ Assertion Failed: " + actual + " !== " + expected)
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`)
   }
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
